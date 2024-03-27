abd9cc26801da0e51199253d2bd79663fa5d3bd2
XenonBird <bird.xenon@gmail.com>
2024-03-28 00:27:35 +0530

    Adds box style and makes it moduler

---
+++ src/questions-in-list.js
@@ -0,0 +1,49 @@
+import {
+  transformIntoHtmlListElement,
+  transformTextToBengali,
+} from '../lib/helpers';
+import { Problem } from '../lib/problem';
+
+const generateButton = document.getElementById('generate-button');
+const clearButton = document.getElementById('clear-button');
+const benButton = document.getElementById('ben');
+const problemType = document.getElementById('problem-type');
+const digit = document.getElementById('digit');
+const outputBox = document.getElementById('output-container');
+
+generateButton.addEventListener('click', () => generate());
+clearButton.addEventListener('click', () => clear());
+
+// functions
+const ifBengali = () => benButton.checked;
+const getProblemType = () => problemType.value;
+const getNumberOfDigit = () => digit.value;
+
+// var problems = [];
+var currentIndex = 0;
+
+const generate = () => {
+  const type = getProblemType();
+  const digit = getNumberOfDigit();
+  const bengali = ifBengali();
+
+  for (let i = 0; i < 10; i++) {
+    var problem = new Problem(type, digit);
+    if (bengali) problem = problem.toBengali();
+    var index = ++currentIndex;
+    if (bengali) index = transformTextToBengali(index.toString());
+
+    const div = transformIntoHtmlListElement(
+      problem.num1,
+      problem.num2,
+      problem.symbol,
+      index
+    );
+    outputBox.appendChild(div);
+  }
+};
+
+const clear = () => {
+  outputBox.innerHTML = '';
+  currentIndex = 0;
+};
